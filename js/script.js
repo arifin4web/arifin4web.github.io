60ae1aa9a01751951bdedbd55a3dd072a9cd81f6
arifin <arifin4web@gmail.com>
2016-03-04 15:36:55 +0600

    - Work and Achievements sections are added.

--- js/script.js
+++ js/script.js
@@ -14,9 +14,32 @@ $(document).ready(function() {
     });
   });
 
+  $('a.goto').each(function() {
+    $(this).click(function() {
+      var goToName = $(this).attr("data-goto");
+      var goToElem = "#"+goToName;
+
+      $('html,body').animate({
+        scrollTop: $(goToElem).offset().top
+      }, 'slow');
+      return false;
+    });
+  });
+
   // Send Mixpanel
   mixpanel.track("homepage_view");
 });
+
+var goto = function(elem){
+  console.log(elem);
+
+  var goToName = $(elem).attr("data-goto");
+  var goToElem = "#"+goToName;
+
+  $('html,body').animate({
+    scrollTop: $(goToElem).offset().top
+  }, 'slow');
+}
 
 var track_resume_download_click = function (){
   mixpanel.track("resume_download");
